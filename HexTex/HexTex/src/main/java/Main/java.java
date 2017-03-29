9301e280431240ddc66a7536f21d3642276d12d2
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2017-03-29 18:57:07 +0300

    Project initialized.

---
+++ HexTex/HexTex/src/main/java/Main/java.java
@@ -0,0 +1,9 @@
+package Main;
+
+public class java {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
---
+++ HexTex/src/main/java/Main.java
@@ -0,0 +1,7 @@
+package hexagon.hextex;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("JEEE");
+	}
+}
