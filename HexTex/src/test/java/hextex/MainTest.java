56097d2c527c9a2053758a82a2b4fc0047e742cb
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2017-03-29 20:48:26 +0300

    One test added.

---
+++ HexTex/src/test/java/hextex/MainTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hextex;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author aleksisvuoksenmaa
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+   @Test
+   public void hello() {
+       assertTrue(true);
+   }
+}
