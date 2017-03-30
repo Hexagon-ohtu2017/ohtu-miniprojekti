b6a87f32988d45796064786efb609683710c4666
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2017-03-30 13:26:47 +0300

    Added plugins and stuff to build.gradle. Removed .DS_Stored.

--- src/main/java/hextex/Main.java
+++ src/main/java/hextex/Main.java
@@ -1,10 +1,22 @@
 package hextex;
 
+import java.util.Scanner;
+
 public class Main {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("JEE TÄÄ TOIMII!!");
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Anna kirjan nimi: ");
+        
+        String nimi = "";
+        if(s.hasNextLine()) {
+            nimi = s.nextLine();
+        }
+        
+        System.out.println("Annoit nimen: " + nimi);
+        
     }
 }
