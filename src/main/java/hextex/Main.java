8e1380ae1da252f6927ccb26ee81a55b248b75af
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2017-03-31 13:55:31 +0300

    Changed the wording in Main.

--- src/main/java/hextex/Main.java
+++ src/main/java/hextex/Main.java
@@ -6,11 +6,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("JEE TÄÄ TOIMII!!");
         ConsoleIO io = new ConsoleIO();
         String name = io.readLine("Give book's name");
         System.out.println("Book's name is " + name);
-        int pages = io.readInt("How many pages book have?");
+        int pages = io.readInt("How many pages does the book have?");
         System.out.println("Book has " + pages + " pages");
     }
 }
