d43768eff38192da797fc0d14e3cdbf5583c87f6
Tuure Piitulainen <tuure.piitulainen@gmail.com>
2017-03-31 16:00:59 +0300

    Add test class for io class

---
+++ src/test/java/hextex/io/IoTest.java
@@ -0,0 +1,42 @@
+package hextex.io;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class IoTest {
+
+    ConsoleIO consoleIO = new ConsoleIO();
+
+    public IoTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+   @Test
+   public void hello() {
+       assertTrue(true);
+   }
+}
