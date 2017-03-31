fec887de7307b63048e43aae53f02427959eac7a
Lehtonen Tuomo T P <tuletule@da5-cs-bk107-18.helsinki.fi>
2017-03-31 15:11:50 +0300

    added field nameOfReference to Book (and some cleaning of code)

--- src/main/java/hextex/io/ConsoleIO.java
+++ src/main/java/hextex/io/ConsoleIO.java
@@ -2,24 +2,21 @@ package hextex.io;
 
 import java.util.Scanner;
 
-
-
 public class ConsoleIO {
 
- private Scanner scanner;
+    private Scanner scanner;
 
- public ConsoleIO() {
-   this.scanner = new Scanner(System.in);
- }
+    public ConsoleIO() {
+        this.scanner = new Scanner(System.in);
+    }
 
+    public void print(String toPrint) {
+        System.out.println(toPrint);
+    }
 
-   public void print(String toPrint) {
-     System.out.println(toPrint);
-   }
-
-   public int readInt(String prompt) {
-      System.out.println(prompt);
-      return Integer.parseInt(scanner.nextLine());
+    public int readInt(String prompt) {
+        System.out.println(prompt);
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public String readLine(String prompt) {
--- src/main/java/hextex/references/Book.java
+++ src/main/java/hextex/references/Book.java
@@ -10,34 +10,58 @@ package hextex.references;
  * @author cocacoca
  */
 public class Book {
-    
+
+    public String nameOfReference;
     public String author;
     public String title;
     public int year;
     public String publisher;
-    
-    public Book(String author, String title, int year, String publisher){
-        this.author=author;
-        this.title=title;
-        this.year=year;
-        this.publisher=publisher;
+
+    public Book(String name, String author, String title, int year, String publisher) {
+        this.nameOfReference = name;
+        this.author = author;
+        this.title = title;
+        this.year = year;
+        this.publisher = publisher;
     }
-    public void setAuthor(String author){
-        this.author=author;
-    }public void setTitle(String title){
-        this.title=title;
-    }public void setYear(int year){
-        this.year=year;
-    }public void setPublisher(String publisher){
-        this.publisher=publisher;
-    }public String getAuthor(){
+
+    public void setName(String name) {
+        this.nameOfReference = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getName() {
+        return nameOfReference;
+    }
+
+    public String getAuthor() {
         return author;
-    }public String getTitle(){
+    }
+
+    public String getTitle() {
         return title;
-    }public int getYear(){
+    }
+
+    public int getYear() {
         return year;
-    }public String getPublisher(){
+    }
+
+    public String getPublisher() {
         return publisher;
     }
-    
 }
